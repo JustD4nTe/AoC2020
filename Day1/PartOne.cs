2baa9d97b4efcc8e3ebed888216a614fa0c6b190
Dominik Tyc <domikxf@gmail.com>
2020-12-01 15:42:10 +0100

    Day 1 Part 1

---
+++ Day1/PartOne.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2020.Day1
+{
+    public static class PartOne
+    {
+        public static int Solve()
+        {
+            using var sr = new StreamReader("data.txt");
+
+            var numbers = sr.ReadToEnd()
+                      .Split("\r\n")
+                      .Select(x => Convert.ToInt32(x));
+
+            var foo = 0;
+
+            foreach (var number in numbers)
+            {
+                foo = 2020 - number;
+
+                if (numbers.Contains(foo))
+                {
+                    foo *= number;
+                    break;
+                }
+            }
+
+            return foo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AoC2020.Day1;
+
+namespace AoC2020
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(PartOne.Solve());
+        }
+    }
+}
